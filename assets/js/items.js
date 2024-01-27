5b7e0ac1768a111d4a5b15d9d72fab52847e568d
zains1234 <yusuf.zain.s@mail.ugm.ac.id>
2024-01-27 12:56:43 +0700

    feat(items): add handling item

--- assets/js/items.js
+++ assets/js/items.js
@@ -2,10 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const clickableCardElements = document.querySelectorAll(
     "#outer-container-items .card-item"
   );
+  const clickableCloseModalElements = document.querySelectorAll(
+    "#modals-container .modal-close-icon"
+  );
 
   clickableCardElements.forEach(function (element) {
     element.addEventListener("click", function () {
-      const card = element
+      const card = element;
       const frontSide = element.querySelector(".front-item");
       const backSide = element.querySelector(".back-item");
 
@@ -14,9 +17,25 @@ document.addEventListener("DOMContentLoaded", function () {
       backSide.classList.toggle("show-item");
     });
   });
+  clickableCloseModalElements.forEach(function (element) {
+    element.addEventListener("click", function () {
+      const modalContainerElement = document.querySelector("#modals-container");
+      // Access the parent node
+      const parentNode = element.parentNode;
+      // Access the grandparent node
+      const grandparentNode = parentNode.parentNode;
+      grandparentNode.classList.toggle("show-item");
+      modalContainerElement.classList.toggle("show-item");
+    });
+  });
 });
 
 function handleElementClick(event) {
-  alert("Clicked on the overlapped element!");
+  const clickedElement = event.target;
+  const nameElement = clickedElement.name;
+  const modalContainerElement = document.querySelector("#modals-container");
+  const modalElement = document.querySelector(`#modal-${nameElement}`);
+  modalContainerElement.classList.toggle("show-item");
+  modalElement.classList.toggle("show-item");
   event.stopPropagation();
 }
